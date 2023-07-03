a336c97f4abc1f5a06026a47182d26ed95e3953d
mapao <paolomta2705@gmail.com>
2023-07-03 05:33:29 -0500

    cambios

--- app/Router/router.js
+++ app/Router/router.js
@@ -1,24 +1,24 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import Login from "../Screens/login";
 import {Menu} from "../Screens/menu";
-import { registrarProduct } from '../Screens/registrarProduct';
+import {registrarProduct} from '../Screens/registrarProduct';
 
 const Stack = createStackNavigator();
 
 export default function Router() {
     return (
         <Stack.Navigator>
-                    <Stack.Screen name="index" component={registrarProduct} options={
+            <Stack.Screen name="index" component={Menu} options={
                 {
                     headerShown: false,
                 }
             }/>
+            {/*<Stack.Screen name="registrar" component={registrarProduct} options={*/}
+            {/*    {*/}
+            {/*        headerShown: false,*/}
+            {/*    }*/}
+            {/*}/>*/}
             <Stack.Screen name="login" component={Login} options={
-                {
-                    headerShown: false,
-                }
-            }/>
-            <Stack.Screen name="menu" component={Menu} options={
                 {
                     headerShown: false,
                 }
--- app/Screens/menu.js
+++ app/Screens/menu.js
@@ -1,85 +1,52 @@
-import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
+import {StyleSheet, Text, View, Image, TouchableOpacity} from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const Menu = (prop) => {
 
-    if (prop.route.params.username === "") {
-        prop.route.params.username = "Usuario";
+    const getProducts  = async () => {
+        
     }
-
-    const ArrayData = [
-        {
-            id: 1,
-            name: 'fierro',
-            descripcion: 'Metal',
-            url: ''
-        }, {
-            id: 2,
-            name: 'madera',
-            descripcion: 'Metal',
-            url: ''
-        }, {
-            id: 3,
-            name: 'clavo',
-            descripcion: 'Metal',
-            url: ''
-        }, {
-            id: 4,
-            name: 'gaaa',
-            descripcion: 'gaaa',
-            url: ''
-        }, {
-            id: 5,
-            name: 'geee',
-            descripcion: 'geee',
-            url: ''
-        }, {
-            id: 6,
-            name: 'giii',
-            descripcion: 'giii',
-            url: ''
-        }, {
-            id: 7,
-            name: 'xddd',
-            descripcion: 'xddd',
-            url: ''
-        }, {
-            id: 8,
-            name: 'adsads',
-            descripcion: 'asdasd',
-            url: ''
-        }, {
-            id: 9,
-            name: 'lñlñklñ',
-            descripcion: 'asdadljkaskld',
-            url: ''
-        }
-    ]
 
     return (
         <View style={styles.container}>
             <LinearGradient colors={['#b876ff', '#8432ff']} style={styles.container}>
                 <View style={styles.heading}>
                     <Text style={styles.text}>
-                        Bienvenido {prop.route.params.username}
+                        Bienvenido {prop?.route?.params?.username || 'Usuario'}
                     </Text>
-                    <Image source={require('../Assets/logo.png')} style={{ width: 50, height: 50 }} />
+                    <Image source={require('../Assets/logo.png')} style={{width: 50, height: 50}}/>
                 </View>
                 <View style={styles.bodycontainer}>
                     <View style={styles.buttoncontainer}>
                         <TouchableOpacity style={styles.buttonRegistrar}>
                             <Text style={styles.buttonText}>Nuevo
-                                <Icon name="plus" size={15} color="#8432ff" />
+                                <Icon name="plus" size={15} color="#b876ff"/>
                             </Text>
                         </TouchableOpacity>
                     </View>
-                    <View>
-                        {/* { ArrayData.map( x => {
-                            return (
-                                <View id={}></View>
-                            )
-                        })} */}
+                    <View style={styles.listView}>
+                        <View style={styles.card}>
+                            <View style={styles.textView}>
+                                <Text style={styles.cardText}>Test</Text>
+                                <Text style={styles.cardDesc}>Producto test</Text>
+                            </View>
+                            <View style={styles.actionsView}>
+                                <Text style={{...styles.cardDesc,marginBottom: 5}}>Stock: 100</Text>
+                                <View style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    gap: 10
+                                }}>
+                                    <TouchableOpacity style={{backgroundColor: 'rgba(255,255,255,0.13)',padding: 5, width: 30, borderRadius: 5}}>
+                                        <Icon name="edit" size={20} color="#ffffffaa"/>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={{backgroundColor: 'rgba(255,255,255,0.13)',padding: 5, width: 30, borderRadius: 5}}>
+                                        <Icon name="trash" size={20} color="#ffffffaa"/>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
                     </View>
                 </View>
             </LinearGradient>
@@ -108,17 +75,18 @@ const styles = StyleSheet.create({
     bodycontainer: {
         marginTop: '10%',
         borderWidth: 1,
-        borderColor: '#ffffff55',
+        borderColor: 'rgba(255,255,255,0.07)',
         height: '50%',
         width: '80%'
     },
     buttonRegistrar: {
         display: 'flex',
-        backgroundColor: '#e1c5ff',
+        backgroundColor: '#e3c7ff',
         borderRadius: 5,
         width: 'max-content',
         height: 'max-content',
-        padding: '2.5%'
+        padding: '2.5%',
+        marginBottom: 10,
     },
     buttoncontainer: {
         flexDirection: 'row',
@@ -126,13 +94,45 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     buttonText: {
-        color: '#8432ff',
+        color: '#4a0998',
         fontWeight: 'bold',
         height: 'max-content',
         gap: 4,
         display: 'flex'
     },
     listView: {
-
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 10,
+    },
+    card: {
+        backgroundColor: '#910fff',
+        borderWidth: 1,
+        borderColor: '#891fff',
+        borderRadius: 5,
+        padding: '2.5%',
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        position: 'relative',
+        gap: 10,
+    },
+    cardText: {
+        color: '#eadcff',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    cardDesc: {
+        color: '#e2cfff',
+    },
+    textView: {
+        width: '66%'
+    },
+    actionsView: {
+        position: 'relative',
+        flex: 1,
+        alignItems: 'flex-end',
+        display: 'flex',
+        justifyContent: 'center',
     }
 });
